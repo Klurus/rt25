3b8895f9b61d212cdaec8a10fc851aa19ab4eb5d
Klurus <162215169+Klurus@users.noreply.github.com>
2025-09-08 01:28:41 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
+
 variable "OS_AUTH_URL" {
         type = string
         sensitive = true
@@ -16,4 +17,20 @@ variable "OS_USERNAME" {
 variable "OS_PASSWORD" {
         type = string
         sensitive = true
+}
+
+variable "vm_game_count" {
+  type=number
+  default=3
+}
+
+# openstack --insecure network list
+variable "network_id" {
+  type=string
+  default="89593658-1677-49f6-a6d3-54ddd5aa2fcd"
+}
+
+variable "subnet_id" {
+  type = string
+  default = "375f4e32-cb82-487c-b042-7388f6161576"
 }
